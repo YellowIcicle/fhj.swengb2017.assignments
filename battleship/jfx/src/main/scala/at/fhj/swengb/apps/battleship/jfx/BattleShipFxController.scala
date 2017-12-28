a893285b72f9dfc3830139228c3bd83c89aff36e
heiderst16 <stefan.heider@edu.fh-joanneum.at>
2017-12-28 12:47:01 +0100

    some testing in test area done

--- battleship/jfx/src/main/scala/at/fhj/swengb/apps/battleship/jfx/BattleShipFxController.scala
+++ battleship/jfx/src/main/scala/at/fhj/swengb/apps/battleship/jfx/BattleShipFxController.scala
@@ -1,26 +1,18 @@
 package at.fhj.swengb.apps.battleship.jfx
 
-import java.io.{File, FilenameFilter}
+import java.io.File
 import java.net.URL
 import java.nio.file.{Files, Paths}
 import java.util.ResourceBundle
 import javafx.fxml.{FXML, Initializable}
-import javafx.scene.control.{Label, Slider, TextArea}
+import javafx.scene.control.{TextArea, Slider, Label}
 import javafx.scene.layout.GridPane
 import javafx.stage.FileChooser
 import javafx.stage.FileChooser.ExtensionFilter
 
-import at.fhj.swengb.apps.battleship.BattleShipProtocol
 import at.fhj.swengb.apps.battleship.model.{BattleField, BattleShipGame, Fleet, FleetConfig, BattlePos}
-/*
-import java.net.URL
-import java.util.ResourceBundle
-import javafx.fxml.{FXML, Initializable}
-import javafx.scene.control.TextArea
-import javafx.scene.layout.GridPane
+import at.fhj.swengb.apps.battleship.BattleShipProtocol
 
-import at.fhj.swengb.apps.battleship.model.{BattleField, BattleShipGame, Fleet, FleetConfig}
- */
 
 class BattleShipFxController extends Initializable {
 
