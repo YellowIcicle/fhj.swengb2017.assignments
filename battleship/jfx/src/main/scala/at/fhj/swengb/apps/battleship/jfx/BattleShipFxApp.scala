4a5c2a55112a222e130bb39ae3fec195ec8bbd6b
heiderst16 <stefan.heider@edu.fh-joanneum.at>
2017-12-28 19:53:15 +0100

    some testing in test area done

--- battleship/jfx/src/main/scala/at/fhj/swengb/apps/battleship/jfx/BattleShipFxApp.scala
+++ battleship/jfx/src/main/scala/at/fhj/swengb/apps/battleship/jfx/BattleShipFxApp.scala
@@ -9,8 +9,7 @@ import scala.util.{Failure, Success, Try}
 
 object BattleShipFxApp {
 
-  //Become initialized when GUI starts
-  var rootStage: Stage = _; //If this stays null, some crazy shit is going on...
+  var rootStage: Stage = _;
 
   def main(args: Array[String]): Unit = {
     Application.launch(classOf[BattleShipFxApp], args: _*)
@@ -20,15 +19,8 @@ object BattleShipFxApp {
 
 class BattleShipFxApp extends Application {
 
-  val fxml = "/at/fhj/swengb/apps/battleship/jfx/battleshipfx.fxml"
-  val css = "/at/fhj/swengb/apps/battleship/jfx/battleshipfx.css"
-
-  val triedRoot = Try(FXMLLoader.load[Parent](getClass.getResource(fxml)))
-  def setSkin(stage: Stage, fxml: String, css: String): Boolean = {
-    stage.setScene(new Scene(new FXMLLoader(getClass.getResource(fxml)).load[Parent]()))
-    stage.getScene.getStylesheets.clear()
-    stage.getScene.getStylesheets.add(css)
-  }
+  val BodyFile = "/at/fhj/swengb/apps/battleship/jfx/battleshipfx.fxml"
+  val triedRoot = Try(FXMLLoader.load[Parent](getClass.getResource(BodyFile)))
 
   override def start(stage: Stage): Unit = {
     BattleShipFxApp.rootStage = stage
@@ -37,7 +29,6 @@ class BattleShipFxApp extends Application {
         stage.setResizable(false)
         stage.setScene(new Scene(root))
         stage.setTitle("Battleship the Game")
-        setSkin(stage,fxml,css)
         stage.show()
       case Failure(e) => e.printStackTrace()
     }
