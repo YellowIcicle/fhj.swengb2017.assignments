1f2afe47d667630ae57e27699b04f0050a469e49
heiderst16 <stefan.heider@edu.fh-joanneum.at>
2017-12-29 12:08:03 +0100

    some testing in test area done

--- battleship/model/src/main/scala/at/fhj/swengb/apps/battleship/BattleShipProtocol.scala
+++ battleship/model/src/main/scala/at/fhj/swengb/apps/battleship/BattleShipProtocol.scala
@@ -1,71 +1,47 @@
 package at.fhj.swengb.apps.battleship
 
-import at.fhj.swengb.apps.battleship.BattleShipProtobuf.BattleShipGame.{
-  Position,
-  Vessel/*,
-  Alignment*/
-}
-import at.fhj.swengb.apps.battleship.model._
+import at.fhj.swengb.apps.battleship.BattleShipProtobuf.BattleShipGame.{Position, Vessel}
+import at.fhj.swengb.apps.battleship.model.{NonEmptyString, _}
+
 import scala.collection.JavaConverters._
 
 object BattleShipProtocol {
 
-  def convert(g: BattleShipGame): BattleShipProtobuf.BattleShipGame = {
-
-    val protoBattleField = BattleShipProtobuf.BattleShipGame
+  /**
+    * Generates a Protobuf Position from given BattlePos position
+    *
+    * @param battlePos
+    * @return
+    */
+  private def convert(battlePos: BattlePos): Position = {
+    Position
       .newBuilder()
-      .setFieldWidth(g.battleField.width)
-      .setFieldHeight(g.battleField.height)
-
-    //Convert vessesls to protobuf-Vessels and add it
-    val fleetProtobuf: Set[Vessel] =
-      g.battleField.fleet.vessels.map(e => convert(e))
-    fleetProtobuf.foreach(e => protoBattleField.addVessels(e))
-
-    //Convert set of BattleBos to Protobuf clicked positions add add it
-    val clickedPos =
-      g.GameState.map(e => convert(e))
-    clickedPos.foreach(e => protoBattleField.addHitCells(e))
-
-    //Build battlefield and write to file
-    protoBattleField.build()
+      .setColumn(battlePos.x)
+      .setRow(battlePos.y)
+      .build()
   }
 
-  def convert(g: BattleShipProtobuf.BattleShipGame): BattleShipGame = {
-    //Create data for BattleField
-    val fleet: Fleet = Fleet(
-      g.getVesselsList.asScala.map(e => convert(e)).toSet)
 
-    val battleField = BattleField(g.getFieldWidth, g.getFieldHeight, fleet)
-
-    //Create set of alread clicked positions
-    val clickedPos: List[BattlePos] =
-      g.getHitCellsList.asScala.map(e => convert(e)).toList
-
-    //Create BattleshipGame and set aready clicked positions
-    val game = BattleShipGame(battleField,
-      e => (),
-      e => (),
-      e => e.toDouble,
-      e => e.toDouble)
-    game.GameState = clickedPos
-
-    //return game
-    game
+  /** Converts a Protobuf Position to a BattleShipGame BattlePos
+    *
+    * @param position
+    * @return
+    */
+  private def convert(position: Position): BattlePos = {
+    BattlePos(position.getColumn, position.getRow)
   }
 
+  
   /**
     * Convertes a given Battleship-Game Vessel to an Protobuf Vessel to store
     *
     * @param vessel
     * @return
     */
-  // Previous convertVesseltoProtobufVessel
   def convert(vessel: at.fhj.swengb.apps.battleship.model.Vessel): Vessel = {
-    //Create new protobuf Vessel
     Vessel.newBuilder()
       .setAlignment(vessel.direction match {
-        case Vertical   => "Vertical";
+        case Vertical => "Vertical";
         case Horizontal => "Horizontal";
         case _ => "Vertical"
       })
@@ -74,8 +50,8 @@ object BattleShipProtocol {
       .setPos(
         Position
           .newBuilder()
-          .setColumn(vessel.InitPosition.x)
           .setRow(vessel.InitPosition.y)
+          .setColumn(vessel.InitPosition.x)
           .build())
       .build()
   }
@@ -86,46 +62,51 @@ object BattleShipProtocol {
     * @param vessel
     * @return
     */
-  //Previous convertProtobufVesseltoVessel
   def convert(vessel: Vessel): at.fhj.swengb.apps.battleship.model.Vessel = {
-
-    val name: NonEmptyString = NonEmptyString(vessel.getName)
-    val startPos: BattlePos =
-      BattlePos(vessel.getPos.getColumn, vessel.getPos.getRow)
-    val size = vessel.getSize
-    val direction: Direction = vessel.getAlignment match {
-      case "Vertical"   => Vertical
-      case "Horizontal" => Horizontal
-      case _ => Vertical
-    }
-
-    //Create Battleship Vessel and return it.
-    at.fhj.swengb.apps.battleship.model.Vessel(name, startPos, direction, size)
+    at.fhj.swengb.apps.battleship.model.Vessel(
+      NonEmptyString(vessel.getName),
+      BattlePos(vessel.getPos.getColumn, vessel.getPos.getRow),
+      vessel.getAlignment match {
+        case "Vertical" => Vertical
+        case "Horizontal" => Horizontal
+        case _ => Vertical},
+      vessel.getSize)
   }
 
-  /**
-    * Generates a Protobuf Position from given BattlePos position
-    *
-    * @param battlePos
-    * @return
-    */
-  //Previous convertBattlePosToProtobufPosition
-  private def convert(battlePos: BattlePos): Position = {
-    Position
+
+  //Converter: Game State => Protobuf Game State
+  def convert(game: BattleShipGame): BattleShipProtobuf.BattleShipGame = {
+    val ProtoField3000 = BattleShipProtobuf.BattleShipGame
       .newBuilder()
-      .setColumn(battlePos.x)
-      .setRow(battlePos.y)
-      .build()
+      .setColumns(game.battleField.width)
+      .setRows(game.battleField.height)
+
+    //Converting the Vessels into Protobuf Format
+    game.battleField.fleet.vessels.map(x => convert(x)).foreach(x => ProtoField3000.addVessels(x))
+
+    //Converting the Cells that were already hit into Protobuf Format
+    game.GameState.map(x => convert(x)).foreach(x => ProtoField3000.addHitCells(x))
+
+    //Convert the Game State into Protobuf Format
+    ProtoField3000.build()
   }
 
-  /** Converts a Protobuf Position to a BattleShipGame BattlePos
-    *
-    * @param position
-    * @return
-    */
-  //previous convertProtoBufPositionToBattlePos
-  private def convert(position: Position): BattlePos = {
-    BattlePos(position.getColumn, position.getRow)
+
+  //Converter: Protobuf Game State => Game State
+  def convert(game: BattleShipProtobuf.BattleShipGame): BattleShipGame = {
+
+    //Rebuilding the whole Game State
+    val BattleShipGame3000 = BattleShipGame(
+      BattleField(game.getColumns, game.getRows, Fleet(game.getVesselsList.asScala.map(e => convert(e)).toSet)),
+      e => (), //Log
+      e => (), //Slider
+      e => e.toDouble, //CellWidth
+      e => e.toDouble) //CellHeight
+
+    //List with Cells that were already hit:
+    val HitCells: List[BattlePos] = game.getHitCellsList.asScala.map(e => convert(e)).toList
+    BattleShipGame3000.GameState = HitCells
+    BattleShipGame3000 //Returning Game State
   }
 
 }
