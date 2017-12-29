3798ec2901d037ee9cedc9cb99eecab302a88514
YellowIcicle <michaelderler@gmx.net>
2017-12-29 16:32:01 +0100

    Battleship Homework

--- battleship/model/src/main/scala/at/fhj/swengb/apps/battleship/model/BattleFxCell.scala
+++ battleship/model/src/main/scala/at/fhj/swengb/apps/battleship/model/BattleFxCell.scala
@@ -2,38 +2,47 @@ package at.fhj.swengb.apps.battleship.model
 
 import javafx.scene.paint.Color
 import javafx.scene.shape.Rectangle
-;
 
 /**
   * Represents one part of a vessel or one part of the ocean.
   */
-
-case class BattleFxCell(pos: BattlePos
-                        , width: Double
-                        , height: Double
-                        , log: String => Unit
-                        , someVessel: Option[Vessel] = None
-                        , fn: (Vessel, BattlePos) => Unit
-                        ) extends Rectangle(width, height) {
+case class BattleFxCell(pos: BattlePos,
+                        width: Double,
+                        height: Double,
+                        log: String => Unit,
+                        someVessel: Option[Vessel] = None,
+                        fn: (Vessel, BattlePos) => Unit,
+                        upClickedPos: BattlePos => Unit)
+  extends Rectangle(width, height) {
 
   def init(): Unit = {
-    if (someVessel.isDefined) {
-      setFill(Color.YELLOWGREEN)
-    } else {
-      setFill(Color.BLUE)
-    }
+    setFill(Color.DARKBLUE)
   }
 
   setOnMouseClicked(e => {
+    if(!isDisable)
+      upClickedPos(pos)
     someVessel match {
       case None =>
-        log(s"Missed. Just hit water.")
-        setFill(Color.AQUAMARINE)
+        log(s"Just hit water!")
+        setFill(Color.MEDIUMAQUAMARINE)
       case Some(v) =>
-        // log(s"Hit an enemy vessel!")
         fn(v, pos)
         setFill(Color.RED)
     }
   })
+
+  def Clicker3000() = {
+    if(!isDisable)
+      upClickedPos(pos)
+    someVessel match {
+      case None =>
+        log(s"Just hit water!")
+        setFill(Color.MEDIUMAQUAMARINE)
+      case Some(v) =>
+        fn(v, pos)
+        setFill(Color.RED)
+    }
+  }
 
 }
